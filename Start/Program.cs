02c5db8d99a636d823f26a3e26355c5e56c20340
Besitzer <Besitzer@DESKTOP-EOROG8D>
2017-08-31 22:26:14 +0200

    Split lines in tuple.

--- Start/Program.cs
+++ Start/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,14 @@ namespace Start
     {
         static void Main(string[] args)
         {
+            var lines = File.ReadAllLines(args[0]).Skip(1);
+            var data = lines.Select(l => ReadData(l));
+        }
+
+        static (string day, string time, string taskId) ReadData(string line)
+        {
+            var words = line.Split(',').Select(w => w.Replace("\"", string.Empty));
+            return (words.ElementAt(0), words.ElementAt(1), words.ElementAt(2));
         }
     }
 }
