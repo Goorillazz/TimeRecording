b7265df0d99d240e05188498b0690f6401af0764
Christian <christian.curilla@publicispixelpark.de>
2017-09-01 08:46:16 +0200

    Create Reader assembly.

---
+++ DataReader/Reader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataReader
+{
+    public class Reader
+    {
+
+
+        public static (string day, string time, string taskId) ReadData(string line)
+        {
+            var words = line.Split(',').Select(w => w.Replace("\"", string.Empty));
+            return (words.ElementAt(0), words.ElementAt(1), words.ElementAt(2));
+        }
+    }
+}
--- Start/Program.cs
+++ Start/Program.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DataReader;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Start
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
             var lines = File.ReadAllLines(args[0]).Skip(1);
-            var data = lines.Select(l => ReadData(l));
+            var data = lines.Select(Reader.ReadData);
         }
 
-        static (string day, string time, string taskId) ReadData(string line)
-        {
-            var words = line.Split(',').Select(w => w.Replace("\"", string.Empty));
-            return (words.ElementAt(0), words.ElementAt(1), words.ElementAt(2));
-        }
     }
 }
