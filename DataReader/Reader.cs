24cd7efa3c05f495be47db85f1bfc6d426b642ac
Besitzer <Besitzer@DESKTOP-EOROG8D>
2017-10-31 15:24:56 +0100

    New Formats

--- DataReader/Reader.cs
+++ DataReader/Reader.cs
@@ -10,10 +10,26 @@ namespace DataReader
     {
 
 
-        public static (string day, string time, string taskId) ReadData(string line)
+        public static (DateTime day, int minutes, int taskId, string taskName) ReadData(string line)
         {
             var words = line.Split(',').Select(w => w.Replace("\"", string.Empty));
-            return (words.ElementAt(0), words.ElementAt(1), words.ElementAt(2));
+
+            var kalenderTag = NewMethod(words.ElementAt(0));
+            var minuten = Convert.ToInt32(words.ElementAt(1));
+            var taskID = Convert.ToInt32(words.ElementAt(2));
+            var taskTitle = words.ElementAt(3);
+
+            return (kalenderTag, minuten, taskID, taskTitle);
+        }
+
+        private static DateTime NewMethod(string datumStr)
+        {
+            var datum = datumStr.Split('.');
+            int jahr = Convert.ToInt32(datum.ElementAt(2).Substring(0, 4));
+            int monat = Convert.ToInt32(datum.ElementAt(1));
+            int tag = Convert.ToInt32(datum.ElementAt(0));
+            var kalenderTag = new DateTime(jahr, monat, tag);
+            return kalenderTag;
         }
     }
 }
--- Start/Program.cs
+++ Start/Program.cs
@@ -7,9 +7,28 @@ namespace Start
     class Program
     {
         static void Main(string[] args)
-        {            
-            var lines = File.ReadAllLines(args[0]).Skip(1);
+        {
+            var path = Properties.Settings.Default.Path;
+            var lines = File.ReadAllLines(path).Skip(1);
             var data = lines.Select(Reader.ReadData);
+
+            var tagesArbeitszeit = data.GroupBy(e => e.day);
+
+            foreach (var item in tagesArbeitszeit)
+            {
+                int sum = 0;
+                
+                foreach(var sdf in item)
+                {
+                    sum = sum + sdf.minutes;
+                }
+                var sdfsdf = item.FirstOrDefault();
+
+                var stunden = sum / 60.0;
+
+                System.Console.WriteLine($"Am {sdfsdf.day.Day}.{sdfsdf.day.Month}.{sdfsdf.day.Year} wurden {stunden} gearbeitet.");
+            }
+            System.Console.ReadLine();
         }
 
     }
